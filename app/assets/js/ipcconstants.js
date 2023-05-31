866a73c57fa1596249507b94ce09f4831bc86fb9
sennecoolgames <81990610+sennecoolgames@users.noreply.github.com>
2023-05-31 13:36:29 +0200

    Update ipcconstants.js

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '80e8f73e-d61b-4f5a-b973-f02a7749c90d'
 // SEE NOTE ABOVE.
 
 
